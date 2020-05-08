a700be7e2ccc1c96240227e84982d76fb56d0bd8
Bluie <60061812+Bluiee@users.noreply.github.com>
2020-05-08 17:32:08 +0100

    Create RockPaperScissors.py

---
+++ RockPaperScissors.py
@@ -0,0 +1,31 @@
+from random import randint
+
+player = input("Rock, paper or scissors?: ").lower()
+rand_num = randint(0,2)
+if rand_num == 0:
+	computer = "rock"
+elif rand_num == 1:
+	computer = "paper"
+else:
+	computer = "scissors"
+print(f"Computer plays {computer}" )
+
+if player == computer:
+	print("It's a draw!")
+elif player == "rock":
+	if computer == "scissors":
+		print("player wins!")
+	else:
+		print("computer wins!")
+elif player == "paper":
+	if computer == "rock":
+		print("player wins!")
+	else:
+		print("computer wins!")
+elif player == "scissors":
+	if computer == "paper":
+		print("player wins!")
+	else:
+		print("computer wins!")
+else:
+	print("Not a valid move. Please try again!")
